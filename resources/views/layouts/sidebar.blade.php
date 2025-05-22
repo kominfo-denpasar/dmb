a157cda89a9ef09b2961ba0ea78f7689fe103f5c
zeneight <thezeneight@gmail.com>
2025-05-22 14:26:18 +0800

    miscs: update tampilan logo sidebar admin

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="{{ route('home') }}" class="brand-link">
+    <a href="{{ route('home') }}" class="brand-link" style="background: #fff;">
         <img src="{{ asset('img/logo_dmb.png') }}"
              alt="AdminLTE Logo"
-             class="elevation-3 img-circle" style="width: 100%;">
+             class="" style="width: 100%;">
     </a>
 
     <div class="sidebar">
