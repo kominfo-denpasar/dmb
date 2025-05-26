c273838e45be7601289921e2c6c70b265c78bb71
zeneight <thezeneight@gmail.com>
2025-05-26 08:20:23 +0800

    feat: ganti tabel menjadi datatable pada modul masyarakat

---
+++ resources/views/masyarakats/datatables_actions.blade.php
@@ -0,0 +1,16 @@
+{!! Form::open(['route' => ['masyarakats.destroy', $id], 'method' => 'delete']) !!}
+<div class='btn-group'>
+    <a href="{{ route('masyarakats.show', $id) }}" class='btn btn-default btn-sm'>
+        <i class="fa fa-eye"></i>
+    </a>
+    <!-- <a href="{{ route('masyarakats.edit', $id) }}" class='btn btn-secondary btn-sm'>
+        <i class="fa fa-edit"></i>
+    </a> -->
+    {!! Form::button('<i class="fa fa-trash"></i>', [
+        'type' => 'submit',
+        'class' => 'btn btn-danger btn-sm',
+        'onclick' => 'return confirm("'.__('crud.are_you_sure').'")'
+
+    ]) !!}
+</div>
+{!! Form::close() !!}
